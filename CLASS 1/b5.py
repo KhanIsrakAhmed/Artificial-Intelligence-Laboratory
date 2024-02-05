b0bcaf09a886a5ad41ab1dfac7f31ee4f461e2f9
Khan Israk Ahmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2024-02-05 21:28:38 +0600

    added class 1

---
+++ CLASS 1/b5.py
@@ -0,0 +1,11 @@
+'''   ****************  Author :  Khan Israk Ahmed  *************************  '''
+'''     ****************  Date:    05-02-2024     *************************  '''
+
+x = -7
+
+if x>0:
+  print('positive')
+elif x==0:
+  print('zero')
+else:
+  print('negative')
---
+++ CLASS 1/b6.py
@@ -0,0 +1,14 @@
+'''   ****************  Author :  Khan Israk Ahmed  *************************  '''
+'''     ****************  Date:    05-02-2024     *************************  '''
+
+x = 15
+
+if 10<x and x<20:
+  print(f'{x} is between 10 and 20')
+
+if 10<x<20:
+  print(f'{x} is between 10 and 20')
+
+
+if x<10 or x>20:
+  print(f"{x} is not between 10 and 20")
