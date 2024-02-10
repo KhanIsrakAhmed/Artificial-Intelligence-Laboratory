b65a3e7a1ef5efd9ca2285d168085cdd965ac586
Khan Israk Ahmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2024-02-10 20:29:02 +0600

    added for ct

--- For CT_1/dict2.py
+++ For CT_1/dict2.py
@@ -6,6 +6,9 @@ list_int = [3, 4, 3, 0, 1, 3, 3, 4, 2, 7, 8, 7]
 
 count_dict = {}
 
+list_int.sort()
+print(list_int)
+
 
 for v in list_int:
   if v in count_dict:
@@ -14,6 +17,9 @@ for v in list_int:
     count_dict[v] = 1
 
 print(count_dict)
+
+list_int.sort(reverse= True)
+print(list_int)
 
 print(max(list_int))
 
